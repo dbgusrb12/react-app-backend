636711cd3f0d3a69a26c8d79ab046b01be53b954
DESKTOP-5EN8VBK\HyunGyu <hg.yu@twolinecode.com>
2020-12-16 12:30:21 +0900

    project init

---
+++ src/main/java/com/example/reactappbackend/ReactAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.reactappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactAppBackendApplication.class, args);
+	}
+
+}
